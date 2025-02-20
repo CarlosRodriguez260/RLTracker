edc692c48c131f0c8938a5f2426252e71a0d5456
CarlosRodriguez260 <carlos.rodriguez260@upr.edu>
2025-02-20 18:12:40 -0400

    Loading Screen

--- src/JavaScript/Tracker.js
+++ src/JavaScript/Tracker.js
@@ -7,6 +7,7 @@ export default function Tracker({epicName}) {
     const params = useParams()
     const url = "https://rocketleague.tracker.network/rocket-league/profile/epic/" + params.epicName + "/overview"
     const [data,setData] = useState([])
+    const [dataEmpty, setDataEmpty] = useState(false)
     useEffect(() => {
       const fetchData = async () => {
         try {
@@ -24,7 +25,7 @@ export default function Tracker({epicName}) {
   
           const data1 = await response.json();
           setData(data1.data);
-          console.log(data); // Handli the response data as needed
+          setDataEmpty(true)
         } catch (error) {
           console.error('There was a problim with the fetch operation:', error);
         }
@@ -35,23 +36,23 @@ export default function Tracker({epicName}) {
     
     return(
       <>
+        <div className="loader">{!dataEmpty && <span>Loading</span>}</div>
+        <div className="Tracker">{dataEmpty &&  
         <div>
-        
-        </div>
-        <div className="Tracker">
-          <header>Stats for {params.epicName}</header>
-          <ul>MMR
-            <li>Casual: {data[0]}</li>
-            <li>1v1: {data[1]}</li>
-            <li>2v2: {data[2]}</li>
-            <li>3v3: {data[3]}</li>
-            <li>Dropshot: {data[4]}</li>
-            <li>Hoops: {data[5]}</li>
-            <li>Rumble: {data[6]}</li>
-            <li>Snowday: {data[7]}</li>
-            <li>Tournament: {data[8]}</li>
-          </ul>
-        </div>
+            <header>Stats for {params.epicName}</header>
+            <ul>MMR
+              <li>Casual: {data[0]}</li>
+              <li>1v1: {data[1]}</li>
+              <li>2v2: {data[2]}</li>
+              <li>3v3: {data[3]}</li>
+              <li>Dropshot: {data[4]}</li>
+              <li>Hoops: {data[5]}</li>
+              <li>Rumble: {data[6]}</li>
+              <li>Snowday: {data[7]}</li>
+              <li>Tournament: {data[8]}</li>
+            </ul>
+          </div>
+        }</div>
       </>
     )
 }
